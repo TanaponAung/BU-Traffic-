f9a3eb35319d6c73dec77700d80ffdb8a21d35d1
Tanapon <tanapon.aung@bumail.net>
2015-11-14 15:00:40 +0700

    Sound Effect Successful

--- app/src/main/java/com/aung/tanapon/butraffic/Traffic.java
+++ app/src/main/java/com/aung/tanapon/butraffic/Traffic.java
@@ -1,13 +1,54 @@
 package com.aung.tanapon.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class Traffic extends AppCompatActivity {
+
+    //Explicit คือการประกาศตัวแปร
+    //การประกาศตัวแปร Private คือใช้แค่ในคลาสใดคลาสหนึ่ง
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traffic);
+
+        //Bind Widget
+        bindWidget(); //การสร้างMethod อัตโนมัติ = ALT+ENTER
+
+        //Button Controller
+        buttonController();
+
+    } //อะไรที่รวม Statement ไว้ คือ Method และนี่คือ Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() { //Set ค่าเมื่อมีการคลิกปุ่ม
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog);
+                buttonPlayer.start();
+
+                //Intent to WebView
+
+
+            } //event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+}  //Main Class นี่คือคลาสหลัก
